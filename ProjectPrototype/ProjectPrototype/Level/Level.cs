5346608166c2ee2824312e802971f2fd30c39b9a
robbie caputo <robbie.caputo@gmail.com>
2010-11-03 17:53:05 -0400

    Added level object

---
+++ ProjectPrototype/ProjectPrototype/Level/Level.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPrototype.Level
+{
+    class Level
+    {
+        public Texture2D sprite;
+        public int distanceTravelled;
+    }
+}
