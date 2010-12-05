449d61c7f98ddaea948125cf7092c9773c6618a1
Bronson Zgeb <Bronson.Zgeb@gmail.com>
2010-12-05 15:59:44 -0500

    Added health bar colors.

--- ProjectPrototype/ProjectPrototype/GameObjects/HealthBar.cs
+++ ProjectPrototype/ProjectPrototype/GameObjects/HealthBar.cs
@@ -21,28 +21,36 @@ namespace ProjectPrototype
 
         public void Draw(SpriteBatch spritebatch, PlayerIndex playerIndex, SpriteFont font)
         {
-            spritebatch.Draw(this.sprite, new Rectangle((int)this.position.X, (int)this.position.Y, sprite.Width, sprite.Height), Color.Gray);
-            spritebatch.Draw(this.sprite, new Rectangle((int)this.position.X, (int)this.position.Y, sprite.Width * CurrentHealth / maxHealth, sprite.Height), Color.Red);
-
             string playerText;
-            switch(playerIndex)
+            Color healthColor;
+
+            switch (playerIndex)
             {
                 case PlayerIndex.One:
+                    healthColor = Color.Red;
                     playerText = "P1";
                     break;
                 case PlayerIndex.Two:
+                    healthColor = Color.Blue;
                     playerText = "P2";
                     break;
                 case PlayerIndex.Three:
+                    healthColor = Color.SaddleBrown;
                     playerText = "P3";
                     break;
                 case PlayerIndex.Four:
+                    healthColor = Color.Goldenrod;
                     playerText = "P4";
                     break;
                 default:
+                    healthColor = Color.Red;
                     playerText = "P1";
                     break;
             }
+
+            spritebatch.Draw(this.sprite, new Rectangle((int)this.position.X, (int)this.position.Y, sprite.Width, sprite.Height), Color.Gray);
+            spritebatch.Draw(this.sprite, new Rectangle((int)this.position.X, (int)this.position.Y, sprite.Width * CurrentHealth / maxHealth, sprite.Height), healthColor);
+
 
             spritebatch.DrawString(font, playerText, this.position, Color.White);
         }
