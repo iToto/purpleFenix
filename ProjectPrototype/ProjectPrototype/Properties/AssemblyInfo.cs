3cb005cf0ce2446d78cdace45489f8c0bcb644a9
Bronson Zgeb <Bronson.Zgeb@gmail.com>
2010-12-07 03:03:48 -0500

    Changed the project name.

--- ProjectPrototype/ProjectPrototype/Properties/AssemblyInfo.cs
+++ ProjectPrototype/ProjectPrototype/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProjectPrototype")]
-[assembly: AssemblyProduct("ProjectPrototype")]
+[assembly: AssemblyTitle("Purple Fenix")]
+[assembly: AssemblyProduct("Purple Fenix")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("zgeb")]
 
